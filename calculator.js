5290c293d167b505c92753adb40c0823d1c6019d
Basin Sonu <basimassis7@gmail.com>
2022-05-18 10:28:54 +0530

    first commit

---
+++ calculator.js
@@ -0,0 +1,55 @@
+// let a = parseInt(prompt("enter the first number"));
+// let b = parseInt(prompt("enter the second number"));
+// const sign = prompt ('select an operator  [+,-,*,/]');
+// if(sign == '+'){
+//     c=a+b;   
+// }
+// else if(sign == '-'){
+//     c=a-b;
+// }
+// else if(sign == '*'){
+//     c=a*b;
+    
+// }
+// else if(sign == '/'){
+//     c=a/b;
+// }
+// else{
+//     console.log("invalid statement");
+// }
+
+// console.log(`${a} ${sign} ${b} = ${c}`);
+
+
+/*
+
+=>  Prompt() is a Condition which is used to popup a blank input box where we can enter the input values.
+=>  while using prompt the entered values will be assigned to the variables as strings, so if we are inputing numbers then we should use parseFloat() or parseInt() outside the prompt.
+    i.e; parseInt(Prompt("statement"));
+=>  To print values of two or more variables in console.log() you can use `${variable name}`
+=>  Prompt() condition doesn't work in vscode,it only works with browsers.
+*/
+
+
+
+let a = 5;
+let b = 10;
+let sign = parseInt
+if(sign == '+'){
+    c=a+b;   
+}
+else if(sign == '-'){
+    c=a-b;
+}
+else if(sign == '*'){
+    c=a*b;
+    
+}
+else if(sign == '/'){
+    c=a/b;
+}
+else{
+    console.log("invalid statement");
+}
+
+console.log(`${a} ${sign} ${b} = ${c}`);
